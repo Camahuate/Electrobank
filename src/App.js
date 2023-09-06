73c230a628c2b8e21ecb4dd6ce0743788472f8ad
Alex <alexgsb15@gmail.com>
2023-09-05 21:29:08 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import LoginForm from './LoginForm'; 
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Login Electrobank</h1>
+        <link rel="stylesheet" href="index.css" />
+      </header>
+      <body>
+        <LoginForm />
+        <script src="script.js"></script>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/LoginForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Electrobank from './imgs/ELECTOBANK-logo.png'
+
+function LoginForm() {
+  const mostrarMensajeYRedirigir = (event) => {
+    event.preventDefault();
+    alert("Acceso concedido");
+    // Lógica para mostrar mensaje y redirigir aquí
+  };  
+
+  return (
+    <div className="body-login">
+      <div className="login-container">
+        <img className="logo" src={Electrobank} alt="logobank" />
+        <form onSubmit={mostrarMensajeYRedirigir} className="form-login">
+          <label htmlFor="usuario">Usuario:</label>
+          <input type="text" id="usuario" placeholder="Usuario" required />
+          <br />
+          <label htmlFor="contrasena">Contraseña:</label>
+          <input type="password" id="contrasena" placeholder="Contraseña" required />
+          <br />
+          <button type="submit" className="btn-violeta">Iniciar Sesión</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
